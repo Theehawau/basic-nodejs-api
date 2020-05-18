ba3d1c8b32b71f2bdb5caf07452bb1046dc89c0e
Toyin Hawau Olamide <hawau.olamide.th@gmail.com>
2020-05-18 04:29:11 +0100

    changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/basic-node-js-api',{ 
+const uri = "mongodb+srv://Theehawau:<nodebasic>@cluster0-grbed.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect('uri',{ 
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://Theehawau:<password>@cluster0-grbed.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+// mongodb://127.0.0.1:27017/basic-node-js-api
