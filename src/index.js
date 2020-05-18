2975db92466e863d0bc0eaa5206df63a8f48a407
Toyin Hawau Olamide <hawau.olamide.th@gmail.com>
2020-05-18 12:46:54 +0100

    y

--- src/index.js
+++ src/index.js
@@ -24,6 +24,40 @@ app.get('/api/users', (req,res) =>{
             err => console.log(err)
         )
 })
+app.get('/api/users/:id', async (req,res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+app.put('/api/users/:id', async (req, res) => {   
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators:true, useFindAndModify:false})
+        if( !user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+app.delete('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id, { useFindAndModify:false})
+        if( !user) {
+            return res.status(404).send()
+         }
+         res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
 app.listen(port, ()=>{
     console.log('Server running on port' +port);
 })
