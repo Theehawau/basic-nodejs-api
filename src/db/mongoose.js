9f14ba9adf90091c8faa73ac6392293193cbf106
Toyin Hawau Olamide <hawau.olamide.th@gmail.com>
2020-05-18 12:26:00 +0100

    y

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV === 'production') {
     const uri = process.env.MONGO_DB_URI
 }else{
-    uri = 'mongodb://127.0.0.1:27017/basic-node-js-api'
+    const uri = 'mongodb://127.0.0.1:27017/basic-node-js-api'
 }
      
 mongoose.connect( uri ,{ 
